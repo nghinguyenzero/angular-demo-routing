084603c16134214cabda982fed9cdcc773ec2190
Nghi Nguyen Vinh <nvnghi@tma.com.vn>
2019-10-24 16:59:18 +0700

    unsubcription

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -1,23 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from './../../models/product.class';
 import { ProductService } from './../../services/product.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
-export class ProductDetailComponent implements OnInit {
+export class ProductDetailComponent implements OnInit, OnDestroy {
   public product: Product = null;
+  public subcription: Subscription;
   constructor(public activateRoute: ActivatedRoute, public productService: ProductService) { }
 
   ngOnInit() {
     // this.handleParamsBySnapShot();
     this.handleParams();
   }
+  ngOnDestroy() {
+    if (this.subcription) {
+      this.subcription.unsubscribe();
+    }
+  }
   handleParamsBySnapShot() {
-    const id = +this.activateRoute.snapshot.params['id'];
+    const id = +this.activateRoute.snapshot.params.id;
     this.product = this.productService.getProductById(id);
   }
   handleParams() {
